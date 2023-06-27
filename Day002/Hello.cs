cefd1042a41f3d39cc6123952368b2090b035492
quilt75 <105329989+quilt75@users.noreply.github.com>
2023-06-27 16:16:27 +0900

    Create Hello.cs
    저장합니다

---
+++ Day002/Hello.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //500-200
+            int a = 500;
+            int b = 200;
+            Console.WriteLine(a - b);
+        }
+    }
+}
